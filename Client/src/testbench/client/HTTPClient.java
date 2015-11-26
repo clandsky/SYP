f9cd7d3652f29c4bceccce870ea817921a66a0c9
Sven <svenmaster89@gmail.com>
2015-11-27 00:25:09 +0100

    http client test eingefügt

---
+++ Client/src/testbench/client/HTTPClient.java
@@ -0,0 +1,26 @@
+package testbench.client;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by Sven on 26.11.15.
+ */
+public class HTTPClient {
+    public static void main(String args[]) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://192.168.0.14/");
+
+        String response = target.path( "testlauf" ).request().accept(MediaType.TEXT_PLAIN).get(String.class);
+
+        String test;
+        test="Der Server der Hurensohn!";
+
+        target.path( "testlauf/hahahaha" ).request().post( Entity.entity(test, MediaType.TEXT_PLAIN),String.class );
+
+        System.out.println(response);
+    }
+}
--- Client/src/testbench/client/Klasse.java
+++
@@ -1,7 +0,0 @@
-package testbench.client;
-
-/**
- * Created by Chrizzle Manizzle on 26.11.2015.
- */
-public class Klasse {
-}
