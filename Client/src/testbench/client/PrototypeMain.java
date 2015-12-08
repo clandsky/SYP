b2e34d3a8159edc56021562c41ddc8855ea76ffe
DrReissdorf <svenmaster89@gmail.com>
2015-12-08 20:36:03 +0100

    keine ahnung

--- Client/src/testbench/client/PrototypeMain.java
+++ Client/src/testbench/client/PrototypeMain.java
@@ -24,17 +24,6 @@ public class PrototypeMain {
         ClientSteuer cSteuer = new ClientSteuer();
 
         if(PrototypeMain.startGUI) {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (UnsupportedLookAndFeelException e) {
-                e.printStackTrace();
-            }
             ClientGUI clientGUI = new ClientGUI();
             clientGUI.setLocationRelativeTo(null);
             clientGUI.setResizable(false);
