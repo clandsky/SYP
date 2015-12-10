37abfb14026055d79fb7a3a3005af1f6ec8b7ee3
Sven <svenmaster89@gmail.com>
2015-12-10 12:44:53 +0100

    iActivateComponentImpl Client eingefügt

--- Client/src/testbench/client/steuerungsklassen/IActivateComponentImpl.java
+++ Client/src/testbench/client/steuerungsklassen/IActivateComponentImpl.java
@@ -1,7 +1,39 @@
 package testbench.client.steuerungsklassen;
 
+import testbench.bootloader.service.IActivateComponent;
+import testbench.client.gui.ClientGUI;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
- * Created by Chrizzle Manizzle on 26.11.2015.
+ * Created by Sven Riedel on 26.11.2015
  */
-public class IActivateComponentImpl {
+public class IActivateComponentImpl implements IActivateComponent {
+    ClientGUI clientGui;
+
+    @Override
+    public boolean startComponent() throws Exception {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        ClientGUI clientGUI = new ClientGUI();
+        clientGUI.setLocationRelativeTo(null);
+        clientGUI.setResizable(false);
+        clientGUI.setTitle("Protobuf Testbench");
+        clientGUI.setBackground(new Color(232,232,232));
+        Dimension d = new Dimension();
+        d.setSize(800,500);
+        clientGUI.setMinimumSize(d);
+        clientGUI.setVisible(true);
+        return true;
+    }
+
+    @Override
+    public JFrame getComponentGui() {
+        return clientGui;
+    }
+
+    @Override
+    public String getCompType() {
+        return "Client";
+    }
 }
