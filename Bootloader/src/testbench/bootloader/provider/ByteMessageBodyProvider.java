2445033a3270a5abc323617a96261941c897edf7
Sven Riedel <svenmaster89@gmail.com>
2015-12-14 02:44:38 +0100

    cleanup

--- Bootloader/src/testbench/bootloader/provider/ByteMessageBodyProvider.java
+++ Bootloader/src/testbench/bootloader/provider/ByteMessageBodyProvider.java
@@ -19,7 +19,6 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
-import java.util.concurrent.Executor;
 
 /**
  *   Created by Christoph Landsky and Sven Riedel (30.11.2015)
@@ -58,51 +57,10 @@ public class ByteMessageBodyProvider implements MessageBodyReader<ByteMessage>, 
     @Override
     public void writeTo(ByteMessage m, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
                         MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
-        ProgressBarWindow pWindow = new ProgressBarWindow();
+        byte[] bArray = m.getByteArray();
 
-        WriteToThread wtt = new WriteToThread(m,pWindow,entityStream);
-        try {
-            wtt.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        pWindow.setVisible(false);
-        pWindow.dispose();
-    }
-
-    public class WriteToThread extends Thread {
-        ByteMessage m;
-        ProgressBarWindow pWindow;
-        OutputStream entityStream;
-
-        public WriteToThread(ByteMessage m, ProgressBarWindow pWindow, OutputStream entityStream) {
-            this.m = m;
-            this.pWindow = pWindow;
-            this.entityStream = entityStream;
-        }
-
-        public void run() {
-            long longWert;
-            byte[] bArray = m.getByteArray();
-
-            for(int i=0 ; i<bArray.length ; i++) {
-                longWert = (long)(i)*100;
-                if(bArray.length<10000000) {
-                    if(longWert%10000==0) {
-                        pWindow.setProgressBar((int)(longWert/bArray.length));
-                    }
-                } else {
-                    if(longWert%1000==0) {
-                        pWindow.setProgressBar((int)(longWert/bArray.length));
-                    }
-                }
-                try {
-                    entityStream.write(bArray[i]);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+        for(int i=0 ; i<bArray.length ; i++) {
+            entityStream.write(bArray[i]);
         }
     }
 }
